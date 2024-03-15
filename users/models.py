f364f669e6787fdd574d8431f1553596e78ffb70
matias <you@example.com>
2024-03-14 22:41:48 -0300

    created class models for PrivateArea and Vehicles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+from vehicles.models import Vehicle
+
+
+class PrivateArea(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
+    allowed_vehicles_type = ArrayField(
+        models.CharField(
+            choices=Vehicle.VEHICLE_TYPES_CHOISES, default="CAR", max_length=25, null=False
+        )
+    )
+    city = models.CharField(max_length=25, null=False)
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Vehicle(models.Model):
+
+    CAR = "CAR"
+    MOTORCYCLE = "MOTORCYCLE"
+    TRUCK = "TRUCK"
+    SAILBOAT = "SAILBOAT"
+    BOAT = "BOAT"
+
+    VEHICLE_TYPES_CHOISES = (
+        (CAR, "Car"),
+        (MOTORCYCLE, "MOTORCYCLE"),
+        (TRUCK, "TRUCK"),
+        (SAILBOAT, "SAILBOAT"),
+        (BOAT, "BOAT"),
+    )
+
+    type = models.CharField(
+        choices=VEHICLE_TYPES_CHOISES, max_length=25, default="CAR", null=False
+    )
+    owner_id = models.IntegerField(null=False)
+    color = models.CharField(max_length=25, null=False)
+    patent = models.CharField(max_length=7, null=False)
+    insurance = models.CharField(max_length=7, null=False)
+    expiration_date = models.DateField(null=False)
+    entry_time = models.DateTimeField(auto_now_add=True, null=False)
+    exit_time = models.DateTimeField(null=True)
+    is_gone = models.BooleanField(default=False)
