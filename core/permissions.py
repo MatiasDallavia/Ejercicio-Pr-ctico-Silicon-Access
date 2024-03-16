a149ab6ebf71d27c5dae103201e43cecf4a1412f
matias <you@example.com>
2024-03-16 18:43:47 -0300

    fixed IsPrivateAreaOwner

--- core/permissions.py
+++ core/permissions.py
@@ -1,15 +1,17 @@
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import BasePermission
 
+from users.models import PrivateArea
+
 
 class IsPrivateAreaOwner(BasePermission):
 
     def has_permission(self, request, view):
-        id = request.parser_context["kwargs"]["area_pk"]
+        area_pk = request.parser_context["kwargs"]["area_pk"]
         user = request.user
-        private_area = user.privatearea_set.filter(id=id).first()
-        print(private_area)
+        private_area = PrivateArea.objects.filter(id=area_pk).first()
 
-        if private_area:
-            return True
-        raise NotFound(f"Private area not found")
+        if private_area is None:
+            raise NotFound("Private area does not exists")
+
+        return private_area.user == user
