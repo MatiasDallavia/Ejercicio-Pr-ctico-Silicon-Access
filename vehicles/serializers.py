72ebaf2390854cca0817d8a330490b982c6f90d1
matias <you@example.com>
2024-03-15 23:15:28 -0300

    created serializer for vehicles

--- vehicles/serializers.py
+++ vehicles/serializers.py
@@ -1,25 +1,55 @@
-from vehicles.models import Vehicle
 from rest_framework import serializers
 
+from constants import VEHICLE_TYPES_CHOISES_DICT
+from users.models import PrivateArea
+from vehicles.models import Vehicle
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
 
+class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
-        fields = ("username", "password", "email", "first_name", "last_name")
+        fields = [
+            "id",
+            "type",
+            "owner_id",
+            "private_area",
+            "color",
+            "patent",
+            "insurance",
+            "expiration_date",
+            "entry_time",
+            "exit_time",
+            "is_gone",
+        ]
 
     def validate(self, attrs):
-        username = attrs.get("username")
-        email = attrs.get("email")
+        print("VALIDATE")
+        type = attrs.get("type")
+        owner_id = attrs.get("owner_id")
+        private_area_id = attrs.get("private_area").id
+        patent = attrs.get("patent")
+        color = attrs.get("color")
 
-        if User.objects.filter(username=username).exists():
-            raise serializers.ValidationError("The username is already in use")
-        if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError("The email is already in use")
+        private_area = PrivateArea.objects.filter(id=private_area_id).first()
+
+        if type not in VEHICLE_TYPES_CHOISES_DICT:
+            raise serializers.ValidationError(f"'{type}' is not a correct vehicle type")
+
+        if type not in private_area.allowed_vehicles_type:
+            raise serializers.ValidationError(
+                f"Vehicles of type '{type}' are not allowed in this private area"
+            )
+
+        if len(patent) != 7 or not all(True for i in patent if i.isalnum()):
+            raise serializers.ValidationError("Patent value has an invalid format")
+
+        if len(color) > 35:
+            raise serializers.ValidationError("Color value is too long")
 
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        print("*" * 30)
+        print(validated_data)
+        vehicle = Vehicle.objects.create(**validated_data)
+        return vehicle
