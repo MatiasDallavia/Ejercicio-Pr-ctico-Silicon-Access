006b43426745301b54b300c79a6c0590de5571b7
matias <you@example.com>
2024-03-15 22:34:03 -0300

    fixed and migrated models

--- users/models.py
+++ users/models.py
@@ -2,14 +2,14 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-from vehicles.models import Vehicle
-
+from constants import VEHICLE_TYPES_CHOISES
 
 class PrivateArea(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
     allowed_vehicles_type = ArrayField(
         models.CharField(
-            choices=Vehicle.VEHICLE_TYPES_CHOISES, default="CAR", max_length=25, null=False
+            choices=VEHICLE_TYPES_CHOISES, default="CAR", max_length=25, null=False
         )
     )
     city = models.CharField(max_length=25, null=False)
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,30 +1,19 @@
 from django.db import models
 
+from constants import VEHICLE_TYPES_CHOISES
+from users.models import PrivateArea
 
-# Create your models here.
+
 class Vehicle(models.Model):
-
-    CAR = "CAR"
-    MOTORCYCLE = "MOTORCYCLE"
-    TRUCK = "TRUCK"
-    SAILBOAT = "SAILBOAT"
-    BOAT = "BOAT"
-
-    VEHICLE_TYPES_CHOISES = (
-        (CAR, "Car"),
-        (MOTORCYCLE, "MOTORCYCLE"),
-        (TRUCK, "TRUCK"),
-        (SAILBOAT, "SAILBOAT"),
-        (BOAT, "BOAT"),
-    )
 
     type = models.CharField(
         choices=VEHICLE_TYPES_CHOISES, max_length=25, default="CAR", null=False
     )
-    owner_id = models.IntegerField(null=False)
-    color = models.CharField(max_length=25, null=False)
+    private_area = models.ForeignKey(PrivateArea, null=False, on_delete=models.DO_NOTHING)
+    owner_id = models.BigIntegerField(null=False)
+    color = models.CharField(max_length=35, null=False)
     patent = models.CharField(max_length=7, null=False)
-    insurance = models.CharField(max_length=7, null=False)
+    insurance = models.CharField(max_length=35, null=False)
     expiration_date = models.DateField(null=False)
     entry_time = models.DateTimeField(auto_now_add=True, null=False)
     exit_time = models.DateTimeField(null=True)
