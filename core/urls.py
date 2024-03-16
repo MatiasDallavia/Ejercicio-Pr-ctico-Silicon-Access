045b3501671747d90adb03ff7b76b4ada5bc5393
matias <you@example.com>
2024-03-16 16:44:08 -0300

    changed endpoints

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,9 @@ from vehicles import views as vi
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', views.UserRegistration.as_view()),
-    path('auth-token/', views.GetToken.as_view()),
+    path('users/auth-token/', views.GetToken.as_view()),
     path('areas/', views.ListPrivateAreaView.as_view()),
-    path('areas/<int:pk>/', views.SinglePrivateAreaView.as_view(), name="area-single"),
-    path('vehicles/', vi.ListVehicleView.as_view(), name="area-single"),
+    path('areas/<int:area_pk>/', views.SinglePrivateAreaView.as_view(), name="area-single"),
+    path('areas/<int:area_pk>/vehicles/', vi.ListVehicleView.as_view(), name="area-single"),
+    path('areas/<int:area_pk>/vehicles/<int:vehicle_pk>/', vi.SingleVehicleView.as_view(), name="area-single"),
 ]
